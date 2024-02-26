cf07d996c87fcfa67956b57a2161f1fbe3d052f2
daite <blueskykind02@yahoo.co.jp>
2024-02-26 20:41:58 +0900

    Update download_video.sh

--- download_video.sh
+++ download_video.sh
@@ -6,6 +6,24 @@ install_ytdlp_python3() {
     python3 -m pip install yt-dlp
 }
 
+# Function to check if a video file already exists
+check_video_exists() {
+    local file_path="downloaded_videos/$1.$2"
+    if [ -f "$file_path" ]; then
+        echo "File already exists: $file_path"
+        return 0  # File exists
+    else
+        return 1  # File does not exist
+    fi
+}
+
+# Function to get title and extension of a video
+get_title_and_ext() {
+    local url="$1"
+    title=$(yt-dlp --get-title "$url")
+    ext=$(yt-dlp --get-filename --restrict-filenames -o '%(ext)s' "$url")
+}
+
 # Check if the operating system is macOS
 if [[ "$(uname)" == "Darwin" ]]; then
     echo "Detected macOS."
@@ -34,7 +52,12 @@ fi
 # Function to download videos asynchronously
 download_videos_async() {
     while IFS= read -r url; do
-        yt-dlp --write-sub -o "downloaded_videos/%(title)s.%(ext)s" "$url" &
+        get_title_and_ext "$url"
+        if check_video_exists "$title" "$ext"; then
+            echo "Skipping download for $title.$ext"
+        else
+            yt-dlp --write-sub -o "downloaded_videos/%(title)s.%(ext)s" "$url" &
+        fi
     done < "$1"
     wait
 }
@@ -48,7 +71,12 @@ if [ $# -eq 1 ]; then
         download_videos_async "$1"
     else
         echo "Downloading video from single URL: $1"
-        yt-dlp --write-sub -o "downloaded_videos/%(title)s.%(ext)s" "$1"
+        get_title_and_ext "$1"
+        if check_video_exists "$title" "$ext"; then
+            echo "Skipping download for $title.$ext"
+        else
+            yt-dlp --write-sub -o "downloaded_videos/%(title)s.%(ext)s" "$1" &
+        fi
     fi
 else
     echo "Usage: $0 input_file.txt or $0 url"

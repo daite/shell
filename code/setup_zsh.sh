60cbbaa7a935e45e0cb0ff4ba344252defe75c8c
daite <blueskykind02@yahoo.co.jp>
2024-05-18 22:34:41 +0900

    Update setup_zsh.sh

--- code/setup_zsh.sh
+++ code/setup_zsh.sh
@@ -10,74 +10,72 @@ fi
 if ! command -v brew &> /dev/null; then
     echo "Homebrew is not installed. Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to install Homebrew."
+        exit 1
+    fi
 fi
 
-# Check if zsh-autosuggestions and zsh-syntax-highlighting are installed
-if ! brew list --formula | grep -q "zsh-autosuggestions"; then
-    echo "Installing zsh-autosuggestions..."
-    brew install zsh-autosuggestions
-fi
+# Array of Homebrew formulae and casks to install
+formulae=("zsh-autosuggestions" "zsh-syntax-highlighting" "python@3" "ffmpeg")
+casks=("vlc" "google-chrome" "qbittorrent" "visual-studio-code")
 
-if ! brew list --formula | grep -q "zsh-syntax-highlighting"; then
-    echo "Installing zsh-syntax-highlighting..."
-    brew install zsh-syntax-highlighting
-fi
+# Install Homebrew formulae
+for formula in "${formulae[@]}"; then
+    if ! brew list --formula | grep -q "$formula"; then
+        echo "Installing $formula..."
+        brew install "$formula"
+        if [[ $? -ne 0 ]]; then
+            echo "Failed to install $formula."
+        fi
+    fi
+done
+
+# Install Homebrew casks
+for cask in "${casks[@]}"; do
+    if ! brew list --cask | grep -q "$cask"; then
+        echo "Installing $cask..."
+        brew install --cask "$cask"
+        if [[ $? -ne 0 ]]; then
+            echo "Failed to install $cask."
+        fi
+    fi
+done
 
 # Check if Oh My Zsh is installed, if not, install it
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     echo "Installing Oh My Zsh..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to install Oh My Zsh."
+        exit 1
+    fi
 fi
 
 # Manually download and configure Powerlevel10k
-echo "Downloading Powerlevel10k..."
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-
-# Add Powerlevel10k to the .zshrc file
-echo "source ~/powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc
-
-# Check and install VLC if not installed
-if ! brew list --cask | grep -q "vlc"; then
-    echo "Installing VLC..."
-    brew install --cask vlc
+if [ ! -d "$HOME/powerlevel10k" ]; then
+    echo "Downloading Powerlevel10k..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to clone Powerlevel10k."
+        exit 1
+    fi
 fi
 
-# Check and install Google Chrome if not installed
-if ! brew list --cask | grep -q "google-chrome"; then
-    echo "Installing Google Chrome..."
-    brew install --cask google-chrome
-fi
-
-# Check and install qBittorrent if not installed
-if ! brew list --cask | grep -q "qbittorrent"; then
-    echo "Installing qBittorrent..."
-    brew install --cask qbittorrent
-fi
-
-# Check and install the latest version of Python 3 if not installed
-if ! brew list --formula | grep -q "python@3"; then
-    echo "Installing the latest version of Python 3..."
-    brew install python@3
-fi
-
-# Check and install ffmpeg if not installed
-if ! brew list --formula | grep -q "ffmpeg"; then
-    echo "Installing ffmpeg..."
-    brew install ffmpeg
+# Add Powerlevel10k to the .zshrc file if not already present
+if ! grep -q "source ~/powerlevel10k/powerlevel10k.zsh-theme" ~/.zshrc; then
+    echo "Adding Powerlevel10k to .zshrc..."
+    echo "source ~/powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc
 fi
 
 # Check and install yt-dlp using pip3 if not installed
-# error: externally-managed-environment; 
 if ! pip3 list | grep -q "yt-dlp"; then
     echo "Installing yt-dlp..."
-    python3 -m pip config set global.break-system-packages true
-    python3 -m pip install yt-dlp
-fi
-
-# Check and install Visual Studio Code if not installed
-if ! brew list --cask | grep -q "visual-studio-code"; then
-    echo "Installing Visual Studio Code..."
-    brew install --cask visual-studio-code
+    python3 -m pip install --break-system-packages yt-dlp
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to install yt-dlp."
+        exit 1
+    fi
 fi
 
 # Reload .zshrc to start Powerlevel10k configuration
